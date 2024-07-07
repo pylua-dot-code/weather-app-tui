1afb3d23d56c29a3e8f90ba1f536b3d07038c756
pyluadotcode <pyluadotcode@gmail.com>
2024-07-07 01:39:17 -0500

    Created base UI

--- pkg/weather-app-tui.go
+++ pkg/weather-app-tui.go
@@ -1,12 +1,36 @@
 package pkg
 
 import (
+	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
 
 func Program() error {
 	app := tview.NewApplication()
-	flex := tview.NewFlex()
+	flex := tview.NewFlex().
+		AddItem(
+			tview.NewBox().
+				SetBorder(true).
+				SetTitleColor(tcell.ColorWhite).
+				SetBorderColor(tcell.ColorWhite).
+				SetTitle("|Locale Selector|").
+				SetBackgroundColor(tcell.ColorDeepSkyBlue),
+			45,
+			1,
+			false,
+		).
+		AddItem(
+			tview.NewBox().
+				SetBorder(true).
+				SetTitleColor(tcell.ColorWhite).
+				SetBorderColor(tcell.ColorWhite).
+				SetTitle("|Weather|").
+				SetBackgroundColor(tcell.ColorDeepSkyBlue),
+			0,
+			1,
+			false,
+		)
+
 	if err := app.SetRoot(flex, true).SetFocus(flex).Run(); err != nil {
 		return (err)
 	}
